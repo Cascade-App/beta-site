8bb5b7f5f871c1cacb5f66d9c95e9b1ff0470a39
Brian Dennis <briandennis321@gmail.com>
2016-06-26 17:02:56 -0500

    prep for download link

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+/* function setElement (platform) {
+  var elements = document.getElementsByClassName('downloadElement');
+  for(var i = 0; i < elements.length; i++){
+    var element = elements[i];
+    if(platform === 'mac'){
+      element.parentNode.href = 'http://www.google.com';
+    }
+    else {
+      element.innerHTML = 'http://google.com/news';
+    }
+  };
+}
 
+var osString = navigator.userAgent.toLowerCase();
+
+if(osString.indexOf('macintosh') !== -1 || osString.indexOf('mac') !== -1) {
+  setElement('mac');
+}
+else {
+  setElement('windows')
+}
+
+/*
